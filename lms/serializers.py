cd509bcd7fe4e02ef374acef6be36d10683ca7e7
Sergey Korchmin <sergey.korchmin@yandex.ru>
2024-10-02 01:23:21 +0300

    Реализован вывод уроков в курсе и список платежей

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,8 +3,15 @@ from rest_framework import serializers
 from lms.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True)
 
     @staticmethod
     def get_lessons_count(obj):
@@ -12,10 +19,4 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = '__all__'
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
         fields = '__all__'
